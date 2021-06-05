136dd6d385c14ac48371cca720e6ae62517989f0
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-06-05 15:06:21 -0300

    Primeiros passos da API

---
+++ core/api/serializers.py
@@ -0,0 +1,10 @@
+from re import M
+from django.db.models import fields
+from rest_framework.serializers import ModelSerializer
+from core.models import PontoTuristico
+
+
+class PontoTuristicoSerializer(ModelSerializer):
+    class Meta:
+        model = PontoTuristico
+        fields = ('id', 'nome', 'descricao')
