8002446368dfc06f9e4f9fe858a277e1ba3b906b
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-06-07 18:04:22 -0300

    Criando o Endpoint de atracoes

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -6,7 +6,7 @@ from rest_framework import routers
 # rest
 from rest_framework import routers
 from core.api.viewsets import PontoTuristicoViewSet
-from atracoes.api.viewsets import AtracoesViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
 
@@ -14,7 +14,7 @@ from comentarios.api.viewsets import ComentarioViewSet
 # rotas rest
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
-router.register(r'atracoes', AtracoesViewSet)
+router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
 
