d5e2faab4b978a24c9f0cc90d050de56954fec58
Luana Rodirgues <luanarodrigues3211@gmail.com>
2021-06-08 15:02:12 -0300

    Filtrando por query string

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from django.db.models import query
 from core.models import PontoTuristico
 
 from rest_framework.response import Response
@@ -7,13 +8,21 @@ from .serializers import PontoTuristicoSerializer
 
 
 class PontoTuristicoViewSet(ModelViewSet):
-    """
-    A simple ViewSet for viewing and editing accounts.
-    """
     serializer_class = PontoTuristicoSerializer
+    #queryset = PontoTuristico.objects.filter()
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        queryset = PontoTuristico.objects.all()
+        if id:
+            queryset = PontoTuristico.objects.filter(id=id)
+        if nome:
+            queryset.filter(nome__iexact=nome)
+        if descricao:
+            queryset.filter(descricao__iexact=descricao)
+        return queryset
 
     def list(self, request, *args, **kwargs):
         return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
